0b21b65b800015b5f2379063020374e2ed095ce8
Michał Wiatr <mwiatr.dev@gmail.com>
2025-06-19 14:54:20 +0200

    fix static export by removing unsupported headers (#14)

--- tests/headers.test.ts
+++ tests/headers.test.ts
@@ -1,7 +1,7 @@
 import config from '../next.config.js';
 
 test('headers configuration', async () => {
-  const headers = await config.headers();
+  const headers = config.customHeaders;
   const linkRule = headers.find(h => h.source === '/:path*');
   expect(linkRule.headers[0].key).toBe('Link');
   expect(linkRule.headers[0].value).toContain('https://images.pexels.com');
